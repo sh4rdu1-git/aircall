52bcfe847a5d44174359c86bb73f0947b418e1c8
Shardul Mulay <mulayshardul99@gmail.com>
2024-01-03 21:35:00 +0530

    refactored ActivityCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./Header.jsx";
 import activities from "./services/activities.js";
-import LogsListView from "./components/CallLog/LogsListView.jsx";
+import ActivityCard from "./components/CallLog/ActivityCard";
 
 const App = () => {
   const [activityLog, setActivityLog] = useState([]);
@@ -11,19 +11,14 @@ const App = () => {
       setActivityLog(initalData);
     });
   }, []);
-  console.log(activityLog);
+
   return (
     <div className="container">
       <Header />
       <div className="container-view">
         Some activities should be here
         {activityLog.map((activity) => {
-          return (
-            // <li key={activity.id}>
-            //   Call from: {activity.from} was {activity.call_type}
-            // </li>
-            <LogsListView key={activity.id} activity={activity} />
-          );
+          return <ActivityCard key={activity.id} activity={activity} />;
         })}
       </div>
     </div>
--- src/components/CallLog/ActivityCard.jsx
+++ src/components/CallLog/ActivityCard.jsx
@@ -2,16 +2,19 @@
 import "../../css/app.css";
 import "../../css/activity.css";
 
-const LogsListView = ({ activity }) => {
+const handleActivityCardClick = () => {};
+
+const ActivityCard = ({ activity }) => {
   let callTypeAltText = `${activity.call_type}_call_icon`;
   return (
     <>
-      <div className="activity-container">
+      <div className="activity-container" onClick={handleActivityCardClick}>
         <img className="activity-icon" src="" alt={callTypeAltText} />
         <div>
           <p>call from {activity.from}</p>
           <p>to {activity.to}</p>
           <p>call type: {activity.call_type}</p>
+          <p>call direction: {activity.direction}</p>
         </div>
         <div>
           <p>creation date: {activity.created_at}</p>
@@ -22,4 +25,4 @@ const LogsListView = ({ activity }) => {
   );
 };
 
-export default LogsListView;
+export default ActivityCard;
